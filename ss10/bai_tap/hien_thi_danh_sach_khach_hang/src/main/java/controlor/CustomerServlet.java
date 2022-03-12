80f6709ab89b3399893549d8105d5187faaf3ccb
phamdangkhoa2411 <jekhoa633@gmail.com>
2022-03-12 21:13:40 +0700

    push

---
+++ ss10/bai_tap/hien_thi_danh_sach_khach_hang/src/main/java/controlor/CustomerServlet.java
@@ -0,0 +1,27 @@
+package controlor;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "CustomerServlet", value = "/Customer")
+public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+CustomerService customerService = new CustomerService();
+        List<Customer> customer = customerService.customerList();
+        request.setAttribute("list",customer);
+        request.getRequestDispatcher("index.jsp").forward(request,response);
+
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
